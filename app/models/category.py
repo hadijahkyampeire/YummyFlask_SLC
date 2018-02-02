62870afd7d371d89bbbde8f775c3e176d3676396
hadijahkyampeire <hadijah315@gmail.com>
2018-02-02 22:10:33 +0300

    fixes

--- app/models/category.py
+++ app/models/category.py
@@ -5,25 +5,45 @@ class Category(object):
     def __init__(self, title):
         self.title = title
         self.categories={}
+        self.recipes={}
+    
+    def add_recipe(self, title, contents, instructions):
+        """ add recipe method"""
+        self.recipes[title] = Recipe(title, contents, instructions)
+        return True
 
-    def add_category(self, title):
-        """ add category method"""
-        if title not in self.categories and title != "" and title != " ":
-            self.categories[title] = Category(title)
-            return True
-        return False
 
-    def edit_category(self, title, new_title):
-        """ edit category method"""
-        if title in self.categories and new_title != "" and new_title !=" ":
-            self.categories[new_title] = self.categories.pop(title)
-            return True
-        return False
+    def edit_recipe(self, title, new_title):
+        """ edit recipe method"""
+        
+        self.recipes[new_title] = self.recipes.pop(title)
+        return True
+        
 
-    def delete_category(self, title):
-        """ delete category method"""
-        if title in self.categories:
-            self.categories.pop(title)
+    def delete_recipe(self, title):
+        """ delete recipe method"""
+        if title in self.recipes:
+            self.recipes.pop(title)
             return True
-        return False
+        return False    
+    # def add_category(self, title):
+    #     """ add category method"""
+    #     if title not in self.categories and title != "" and title != " ":
+    #         self.categories[title] = Category(title)
+    #         return True
+    #     return False
+
+    # def edit_category(self, title, new_title):
+    #     """ edit category method"""
+    #     if title in self.categories and new_title != "" and new_title !=" ":
+    #         self.categories[new_title] = self.categories.pop(title)
+    #         return True
+    #     return False
+
+    # def delete_category(self, title):
+    #     """ delete category method"""
+    #     if title in self.categories:
+    #         self.categories.pop(title)
+    #         return True
+    #     return False
     
--- app/models/user.py
+++ app/models/user.py
@@ -7,5 +7,25 @@ class User(object):
         self.lastname = lastname
         self.email = email
         self.password = password
-      
-    
+        self.categories={} 
+
+    def add_category(self, title):
+        """ add category method"""
+        if title not in self.categories and title != "" and title != " ":
+            self.categories[title] = Category(title)
+            return True
+        return False
+
+    def edit_category(self, title, new_title):
+        """ edit category method"""
+        if title in self.categories and new_title != "" and new_title !=" ":
+            self.categories[new_title] = self.categories.pop(title)
+            return True
+        return False
+
+    def delete_category(self, title):
+        """ delete category method"""
+        if title in self.categories:
+            self.categories.pop(title)
+            return True
+        return False
